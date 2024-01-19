0c690de64d6037362a608547fc7611bf3dfb3853
MuhammadAnas1506 <145413626+MuhammadAnas1506@users.noreply.github.com>
2024-01-19 17:12:02 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,3 @@
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_cardgame/utils/game_utils.dart';
-import 'package:flutter_cardgame/widgets/quiz_app_bar.dart';
-import 'package:flutter_cardgame/widgets/quiz_body.dart';
-import 'package:lottie/lottie.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  //setting text style
-  bool hideTest = false;
-  final Game _game = Game();
-
-  bool isMatch = false;
-  bool isAlreadyAnswered = false;
-
-  //game stats
-  int tries = 0;
-  int score = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _game.initGame();
-  }
-
-  void displayDialog(BuildContext context) {
-    showDialog<String>(
-        barrierColor: Colors.transparent,
-        barrierDismissible: false,
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              icon: LottieBuilder.asset(
-                "assets/correct_animation.json",
-                fit: BoxFit.contain,
-                repeat: false,
-              ));
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      drawer: Drawer(),
-      appBar: QuizAppBar(),
-      body: Center(
-        child: QuizBody(
-          matchingQuiz: MatchingMemory(),
-        ),
-      ),
-      // bottomNavigationBar: QuizBottomBar(),
-    );
-  }
-}
-
-class MatchingMemory extends StatefulWidget {
-  const MatchingMemory({Key? key}) : super(key: key);
-
-  @override
-  State<MatchingMemory> createState() => _MatchingMemoryState();
-}
-
-class _MatchingMemoryState extends State<MatchingMemory> {
-  // settings for the text style
   TextStyle whiteText = const TextStyle(color: Colors.white);
   bool hideTest = false;
   final Game _game = Game();
@@ -98,6 +9,7 @@ class _MatchingMemoryState extends State<MatchingMemory> {
   Map<String, dynamic>? answerCheck1;
   Map<String, dynamic>? answerCheck2;
   bool isDark = false;
+
   //game stats
   int score = 0;
 
@@ -140,52 +52,55 @@ class _MatchingMemoryState extends State<MatchingMemory> {
                 height: MediaQuery.of(context).size.shortestSide,
                 width: MediaQuery.of(context).size.width,
                 child: GridView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: _game.cards_list.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 8.0,
-                      mainAxisSpacing: 8.0,
-                    ),
-                    padding: const EdgeInsets.all(12.0),
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () {
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: _game.cards_list.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                  ),
+                  padding: const EdgeInsets.all(8.0),
+                  itemBuilder: (context, index) {
+                    return GestureDetector (
+                      onTap: () {
+                        setState(() {
+                          isMatch[index] = true;
+                          if (answerCheck1 == null) {
                           setState(() {
-                            isMatch[index] = true;
-                            if (answerCheck1 == null) {
-                              setState(() {
-                                answerCheck1 = {
-                                  "color": _game.cards_list[index].color,
-                                  "index": index
-                                };
-                              });
-                            } else if (answerCheck2 == null) {
-                              setState(() {
-                                answerCheck2 = {
-                                  "color": _game.cards_list[index].color,
-                                  "index": index
-                                };
-                              });
-                            }
-
-                            if (answerCheck2 != null && answerCheck2 != null) {
-                              if (answerCheck1!['color'] !=
-                                  answerCheck2!['color']) {
+                            answerCheck1 = {"color": _game.cards_list[index].color,"index": index};
+                          });
+                        } else if (answerCheck2 == null) {
+                            setState(() {
+                            answerCheck2 = {"color": _game.cards_list[index].color,"index": index};
+                          });
+                          }
+                          if (answerCheck2 != null && answerCheck2 != null) {
+                              if (answerCheck1!['color'] != answerCheck2!['color']) {
                                 ///when wrong answer reset isMatch
-                                Future.delayed(Duration(milliseconds: 500), () {
+                                Future.delayed(
+                                    const Duration(milliseconds: 500), () {
                                   setState(() {
                                     isMatch[answerCheck1!['index']] = false;
                                     isMatch[answerCheck2!['index']] = false;
                                     answerCheck1 = null;
                                     answerCheck2 = null;
+                                    AssetsAudioPlayer.newPlayer().open(
+                                      Audio(
+                                          "assets/audios/wrong_sound.mp3"),
+                                      autoStart: true,
+                                      showNotification: true,
+                                    );
                                   });
                                 });
-
                                 print("reseting answered cards");
                               } else {
                                 ///when true answer reset current choice
+                                AssetsAudioPlayer.newPlayer().open(
+                                  Audio(
+                                      "assets/audios/right_sound.mp3"),
+                                  autoStart: true,
+                                  showNotification: true,
+                                );
                                 setState(() {
                                   answerCheck1 = null;
                                   answerCheck2 = null;
@@ -193,69 +108,80 @@ class _MatchingMemoryState extends State<MatchingMemory> {
                                 print("proceed next choice");
                               }
                             }
-                          });
-                          // print(answerCheck2);
-                          // print(answerCheck1);
+
                           if (!isMatch.contains(false)) {
+                            AssetsAudioPlayer.newPlayer().open(
+                              Audio(
+                                  "assets/audios/congratulation_sound.mp3"),
+                              autoStart: true,
+                              showNotification: true,
+                            );
                             showDialog(
                                 context: context,
                                 barrierDismissible: false,
                                 builder: (_) => Dialog(
-                                      backgroundColor: Colors.transparent,
-                                      elevation: 0,
-                                      child: Container(
-                                        alignment: FractionalOffset.center,
-                                        height: 300.0,
-                                        padding: const EdgeInsets.all(20.0),
-                                        child: Lottie.asset(
-                                          "assets/correct_animation.json",
-                                          fit: BoxFit.contain,
-                                          repeat: false,
-                                        ),
-                                      ),
-                                    ));
+                                  backgroundColor: Colors.transparent,
+                                  elevation: 0,
+                                  child: Container(
+                                    alignment: FractionalOffset.center,
+                                    height: 300.0,
+                                    padding: const EdgeInsets.all(20.0),
+                                    child: Lottie.asset(
+                                      "assets/correct_animation.json",
+                                      fit: BoxFit.contain,
+                                      repeat: false,
+                                    ),
+                                  ),
+                                ));
                             Future.delayed(const Duration(seconds: 2), () {
                               Navigator.pop(context);
                             });
                           }
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.all(4.0),
-                          padding: const EdgeInsets.all(8.0),
-                          decoration: ShapeDecoration(
-                            color: !isMatch[index]
-                                ? isDark
-                                    ? Colors.grey
-                                    : Colors.orange
-                                : _game.cards_list[index].color,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            shadows: const [
-                              BoxShadow(
-                                color: Color(0x33FFC107),
-                                blurRadius: 8,
-                                offset: Offset(0, 4),
-                                spreadRadius: 2,
-                              )
-                            ],
+                        });
+                        // print(answerCheck2);
+                        // print(answerCheck1);
+                      },
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                        margin: EdgeInsets.all(!isMatch[index] ? 1.0 : 4.0),
+                        padding: const EdgeInsets.all(6.0),
+                        decoration: ShapeDecoration(
+                          color: !isMatch[index]
+                              ? isDark
+                              ? Colors.grey
+                              : Colors.orange
+                              : _game.cards_list[index].color,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(!isMatch[index] ? 8.0 : 14.0),
                           ),
-                          child: Center(
-                            child: AutoSizeText(
-                              !isMatch[index]
-                                  ? ""
-                                  : _game.cards_list[index].name!,
-                              minFontSize: 16,
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
+                          shadows: [
+                            BoxShadow(
+                              color: !isMatch[index] ? const Color(0x33FFC107) : const Color(0x33000000),
+                              blurRadius: !isMatch[index] ? 8.0 : 4.0,
+                              offset: const Offset(0, 4),
+                              spreadRadius: 2,
+                            )
+                          ],
+                        ),
+                        child: Center(
+                          child: AutoSizeText(
+                            !isMatch[index]
+                                ? ""
+                                : _game.cards_list[index].name!,
+                            minFontSize: 16,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w700,
                             ),
                           ),
                         ),
-                      );
-                    }))),
+                      ),
+                    );
+                  }
+                )
+            )
+        ),
       ],
     );
   }
-}
